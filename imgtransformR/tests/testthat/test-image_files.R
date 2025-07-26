bf51282006ab7d639848da2c8303a1b0a9ac8f75
Aayan Khan <aayan.khan02072007@gmail.com>
2025-07-26 18:08:12 +0530

    Initial

--- imgtransformR/tests/testthat/test-image_files.R
+++ imgtransformR/tests/testthat/test-image_files.R
@@ -1,6 +1,6 @@
 test_that("Inavlid image files are stopped", {
   expect_error(open_image("hello.tsv"))
   expect_error(open_image("world.jpYG"))
-  expect_error(save_image(c(),"world.jpYG"))
-  expect_error(save_image(c(),"hello.tsv"))
+  expect_error(save_image(c(), "world.jpYG"))
+  expect_error(save_image(c(), "hello.tsv"))
 })
