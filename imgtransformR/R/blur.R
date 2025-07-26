26bca89a3fc343edc70d4431b159b5df44689a89
Aayan Khan <aayan.khan02072007@gmail.com>
2025-07-26 13:39:39 +0530

    Initial

--- imgtransformR/R/blur.R
+++ imgtransformR/R/blur.R
@@ -5,13 +5,12 @@ blur_image <- function(img_array, box = 3) {
   tmp <- img_array
   height <- dim(img_array)[1]
   width <- dim(img_array)[2]
-  kernel <- matrix(1, box, box) / (box**2)
   shift <- floor((box - 1) / 2)
   for (row in (1 + shift):(height - shift)) {
     for (column in (1 + shift):(width - shift)) {
       for (channel in 1:3) {
         pixel_region <- tmp[(row - shift):(row + shift), (column - shift):(column + shift), channel]
-        img_array[row, column, channel] <- pmin(pmax(sum(pixel_region * kernel), 0), 1)
+        img_array[row, column, channel] <- pmin(pmax(sum(pixel_region)/(box**2), 0), 1)
       }
     }
   }
