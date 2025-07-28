29dec0d7882e31a6404e90e90024ab35cd393c69
Aayan Khan <aayan.khan02072007@gmail.com>
2025-07-28 19:48:25 +0530

    Initialize

--- imgtransformR/R/sharpen.R
+++ imgtransformR/R/sharpen.R
@@ -2,7 +2,7 @@ sharpen_image <- function(img_array) {
   height <- dim(img_array)[1]
   width <- dim(img_array)[2]
   tmp <- img_array
-  kernel <- matrix(c(0, 1, 0, -1, 5, -1, 0, -1, 0), 3, 3)
+  kernel <- matrix(c(0, -1, 0, -1, 5, -1, 0, -1, 0), 3, 3)
   for (row in 2:(height - 1)) {
     for (column in 2:(width - 1)) {
       for (channel in 1:3) {
